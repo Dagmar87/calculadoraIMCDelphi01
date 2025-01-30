07ef2e46e86f14f0736727599895ba3358255d33
Dagmar87 <jdfssobrinho@hotmail.com>
2025-01-30 11:07:48 -0300

    desenvolvimento do codigo do projeto

--- calculadoraIMC.pas
+++ calculadoraIMC.pas
@@ -14,8 +14,11 @@ type
     EditAltura: TEdit;
     ButtonCalcular: TButton;
     LabelAltura: TLabel;
+    procedure ButtonCalcularClick(Sender: TObject);
   private
     { Private declarations }
+    function CalcularIMC(Peso, Altura: Double): Double;
+    function ClassificarIMC(IMC: Double): String;
   public
     { Public declarations }
   end;
@@ -26,5 +29,34 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.ButtonCalcularClick(Sender: TObject);
+var
+  Peso, Altura, IMC: Double;
+  Classificacao: String;
+begin
+  Peso := StrToFloat(EditPeso.Text);
+  Altura := StrToFloat(EditAltura.Text);
+  IMC := CalcularIMC(Peso, Altura);
+  Classificacao := ClassificarIMC(IMC);
+  LabelResultado.Caption := Format('Seu IMC é: %.2f - %s', [IMC, Classificacao]);
+end;
+
+function TForm1.CalcularIMC(Peso, Altura: Double): Double;
+begin
+  Result := Peso / (Altura * Altura);
+end;
+
+function TForm1.ClassificarIMC(IMC: Double): string;
+begin
+  if IMC < 18.5 then
+    Result := 'Abaixo do peso'
+  else if IMC < 24.9 then
+    Result := 'Peso normal'
+  else if IMC <29.9 then
+    Result := 'Sobrepeso'
+  else
+    Result := 'Obesidade';
+end;
 
 end.
