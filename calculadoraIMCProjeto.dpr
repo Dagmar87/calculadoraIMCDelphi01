c1102618e3cd5101ea7e96582d26fa32c536795f
Dagmar87 <jdfssobrinho@hotmail.com>
2025-01-30 10:26:52 -0300

    criação do projeto

---
+++ calculadoraIMCProjeto.dpr
@@ -0,0 +1,14 @@
+program calculadoraIMCProjeto;
+
+uses
+  Vcl.Forms,
+  calculadoraIMC in 'calculadoraIMC.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
